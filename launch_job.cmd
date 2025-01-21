5264341b94f196cfcca4c088db878c672b3d3407
hanyangclarence <mahlerrrr76@gmail.com>
2025-01-21 08:48:01 +0800

    update sbatch command

--- launch_job.cmd
+++ launch_job.cmd
@@ -4,6 +4,8 @@
 #SBATCH -e output_logs/tr_%j.err
 #SBATCH --gres=gpu:1
 
+source /research/d2/fyp24/hyang2/anaconda3/etc/profile.d/conda.sh
+
 conda activate 3d_diffuser_actor
 
 xvfb-run python train.py --stage train_vqvae --base configs/train_motion_vqvae_v2.yaml
