d54864e327cd8a6c294d30c379ad9c80861d6bcf
hanyangclarence <mahlerrrr76@gmail.com>
2025-01-29 12:31:45 +0800

    update

--- launch_job.cmd
+++ launch_job.cmd
@@ -1,13 +1,11 @@
 #!/bin/bash
 #SBATCH --job-name=yh
-#SBATCH --nodes=4                 # total number of nodes
-#SBATCH --ntasks-per-node=1       # 1 task per node
-#SBATCH --gres=gpu:1              # 1 GPU per node
 #SBATCH -o output_logs/tr_%j.out
 #SBATCH -e output_logs/tr_%j.err
-#SBATCH --nodelist=gpu[39-51]
+#SBATCH --gres=gpu:1
 
 source /research/d2/fyp24/hyang2/anaconda3/etc/profile.d/conda.sh
+
 conda activate 3d_diffuser_actor
 
-xvfb-run python train.py --stage train_vqvae --base configs/train_motion_vqvae_no_chunk.yaml
+xvfb-run python train.py --stage train_vqvae --base configs/train_motion_vqvae_no_chunk.yaml -r /research/d2/fyp24/hyang2/fyp/code/fyp_project/training_logs/2025-01-27T11-03-39_train_motion_vqvae_no_chunk/checkpoints/last.ckpt
