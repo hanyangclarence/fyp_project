ed065fd067403eafa2434c15af41ac9f81d9cc70
hanyangclarence <mahlerrrr76@gmail.com>
2025-01-30 09:47:07 +0800

    update

--- launch_job.cmd
+++ launch_job.cmd
@@ -8,4 +8,4 @@ source /research/d2/fyp24/hyang2/anaconda3/etc/profile.d/conda.sh
 
 conda activate 3d_diffuser_actor
 
-xvfb-run python train.py --stage train_vqvae --base configs/train_motion_vqvae_no_chunk.yaml -r /research/d2/fyp24/hyang2/fyp/code/fyp_project/training_logs/2025-01-27T11-03-39_train_motion_vqvae_no_chunk/checkpoints/last.ckpt
+xvfb-run python train.py --stage train_vqvae --base configs/train_motion_vqvae_no_chunk_larger.yaml
