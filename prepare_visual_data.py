61ae685130025e676b08d0e6c350f860976b2a52
hanyangclarence <mahlerrrr76@gmail.com>
2025-03-21 13:31:03 +0800

    update

--- prepare_visual_data.py
+++ prepare_visual_data.py
@@ -17,8 +17,8 @@ from unimumo.rlbench.utils_with_rlbench import RLBenchEnv, keypoint_discovery, i
 MIN_DELTA_TRANS = 0.05
 MIN_DELTA_ROT = 0.4
 
-motion_code_dir_name = "motion_code_v14"
-save_filename = "observations_v14"
+motion_code_dir_name = "motion_code_v21"
+save_filename = "observations_v21"
 
 class MotionVQVAEDataset(Dataset):
     def __init__(
